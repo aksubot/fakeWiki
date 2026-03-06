c5c7b3821294dc29fddf12a8ae76a0362be4e3f4
aksubot <->
2026-03-06 12:37:59 +0200

    lesgoo

---
+++ app.js
@@ -0,0 +1,33 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks =document.querySelector('.navbar__menu');
+const overlay = document.querySelector(".overlay");
+
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    overlay.classList.toggle("overlay-is-active");
+
+
+});
+
+overlay.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+
+    overlay.classList.toggle("overlay-is-active");
+
+});
+
+
+const btns = document.querySelectorAll('.kysBtn');
+const vastaukset = document.querySelectorAll('.kysymysCont');
+const arrows = document.querySelectorAll('.arrow');
+
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+
+        vastaukset[index].classList.toggle('open');
+        arrows[index].classList.toggle('rotate');
+    })
+});
